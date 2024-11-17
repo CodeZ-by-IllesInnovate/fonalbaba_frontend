690e143a4b000a1aa8793cc1ee880c9728a87319
akiilles05 <112715362+akiilles05@users.noreply.github.com>
2024-11-17 10:05:00 +0100

    Update mintak layout

--- app/mintak/page.jsx
+++ app/mintak/page.jsx
@@ -39,43 +39,48 @@ const MintakPage = () => {
   return (
     <section className="mt-20 font-mono">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold mb-6 my-8 tracking-wide text-yellow-600">
+        <h2 className="text-3xl font-bold mb-8 tracking-wide text-yellow-600 text-center">
           Minták
         </h2>
-        <div className="flex flex-wrap-3 gap-6">
-          {patterns.map((pattern) => (
-            <div
-              key={pattern.id}
-              className="relative flex flex-col text-yellow-600 bg-white shadow-lg rounded-xl w-80 text-center"
-            >
-              {/* Képek megjelenítése, ha van elérhető kép */}
-              {pattern.images.length > 0 && (
-                <div className="relative h-[25rem] mx-4 my-4 overflow-hidden shadow-lg rounded-xl">
-                  <img
-                    src={`${process.env.NEXT_PUBLIC_URL}${pattern.images[0]}`}
-                    alt={pattern.title}
-                    className="object-cover w-full h-full"
-                  />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {/* Az elemek ABC sorrendbe rendezése */}
+          {patterns
+            .sort((a, b) => a.title.localeCompare(b.title)) // Rendezés a title alapján
+            .map((pattern) => (
+              <div
+                key={pattern.id}
+                className="relative flex flex-col bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition-shadow"
+              >
+                {/* Képek megjelenítése, ha van elérhető kép */}
+                {pattern.images.length > 0 && (
+                  <div className="relative h-[15rem] w-full overflow-hidden rounded-t-2xl">
+                    <img
+                      src={`${process.env.NEXT_PUBLIC_URL}${pattern.images[0]}`}
+                      alt={pattern.title}
+                      className="object-contain w-full h-full"
+                    />
+                  </div>
+                )}
+                <div className="flex flex-col justify-between flex-grow p-6">
+                  <div>
+                    <h5 className="text-xl font-semibold text-gray-800">
+                      {pattern.title}
+                    </h5>
+                    <ReactMarkdown className="text-sm text-gray-600 leading-relaxed">
+                      {pattern.leiras}
+                    </ReactMarkdown>
+                  </div>
+                  <div className="mt-4">
+                    <Link
+                      href={`/mintak/${pattern.slug}`}
+                      className="block px-6 py-2 text-sm font-medium text-white bg-yellow-600 rounded-full transition-all hover:bg-yellow-700 focus:ring focus:ring-yellow-300 text-center"
+                    >
+                      Részletek
+                    </Link>
+                  </div>
                 </div>
-              )}
-              <div className="p-2">
-                <h5 className="mb-2 text-xl font-semibold leading-snug text-gray-900">
-                  {pattern.title}
-                </h5>
-                <ReactMarkdown className="text-base leading-relaxed text-left">
-                  {pattern.leiras}
-                </ReactMarkdown>
               </div>
-              <div className="p-6 pt-0">
-                <Link
-                  href={`/mintak/${pattern.slug}`}
-                  className="block text-center px-6 py-3 text-xs font-bold text-textColor uppercase bg-gradient-to-r from-primary to-secondary rounded-lg shadow-md transition-all hover:shadow-lg focus:opacity-85 active:opacity-85"
-                >
-                  Részletek
-                </Link>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </section>
